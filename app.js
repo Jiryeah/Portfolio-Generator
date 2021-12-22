3f0d0afdecd11b119063db3256dd378c0d71c66b
Chidori <business.faissal@outlook.com>
2021-12-21 19:01:26 -0600

    minor html generationg with temperal literals

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+
+const generatePage = (userName, githubName) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+}  
+
+console.log(name, github);
+console.log(generatePage(name, github));
 
 
 
@@ -30,17 +54,16 @@ console.log(profileDataArgs);
 
 
 
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+//   console.log('================');
 
-  console.log('================');
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
